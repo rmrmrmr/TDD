d46d8a94ff685952e5906c5b2adb27d1f82f02c7
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-17 10:29:47 -0600

    Add Test case for positive N factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,4 +2,14 @@ require 'rspec'
 require_relative 'solver'
 
 RSpec.describe Solver do
+  describe '#factorial' do
+    context 'when the input is 10' do
+      subject { described_class.new(N: 10) }
+
+      it 'returns 3628800' do
+        subject.factorial
+        expect(subject).to eql(3628800)
+      end
+    end
+  end
 end
