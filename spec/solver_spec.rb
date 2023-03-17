548c469d148beabee3af178fb5a639d3a486acde
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-17 10:58:25 -0600

    Add test cases for factorial with 0 or negative int inputs

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,8 +7,23 @@ RSpec.describe Solver do
       subject { described_class.new(10) }
 
       it 'returns 3628800' do
-        subject.factorial
         expect(subject.factorial).to eql(3628800)
+      end
+    end
+
+    context 'when the input is 0' do
+      subject { described_class.new(0) }
+
+      it 'returns 1' do
+        expect(subject.factorial).to eql(1)
+      end
+    end 
+
+    context 'when the input is a negative integer' do
+      subject { described_class.new(-10) }
+
+      it 'returns "Please enter a positive integer"' do
+        expect(subject.factorial).to eql('Please enter a positive integer')
       end
     end
   end
