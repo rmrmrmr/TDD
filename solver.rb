b2ce86ff20703e33dab09b29cd0139f0c543e40b
PrangonGhose <prangonghose@gmail.com>
2023-03-17 23:31:14 +0600

    Add method to reverse string

--- solver.rb
+++ solver.rb
@@ -1,15 +1,22 @@
 class Solver
-  def initialize(number)
+  def initialize(number = 0, word = '')
     @number = number
+    @word = word
   end
 
   def factorial
     if @number.zero?
       1
-    elsif @nnumber.negative?
+    elsif @number.negative?
       'Please enter a positive integer'
     else
       @number * Solver.new(@number - 1).factorial
     end
+  end
+
+  def reverse
+    any_arr = @word.chars
+    any_arr.reverse!
+    any_arr.join
   end
 end
