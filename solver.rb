5d53c1ca0b1cfb280e62ad080857750f26ba8c85
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-17 10:51:55 -0600

    Finish factorial method for positive integers

--- solver.rb
+++ solver.rb
@@ -1,8 +1,17 @@
 class Solver
-  def initialize(n: 0)
+  def initialize(n)
     @n = n
   end
 
   def factorial
+    result = if @n == 0
+      1
+    else
+      @n * Solver.new(@n-1).factorial
+    end
+    return result
   end
 end
+
+fac = Solver.new(10)
+puts fac.factorial
