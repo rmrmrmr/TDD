a265247020e9a94d23136eda2a85754869a9c3ce
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-17 10:51:32 -0600

    Fix solver_spec

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,11 +4,11 @@ require_relative '../solver'
 RSpec.describe Solver do
   describe '#factorial' do
     context 'when the input is 10' do
-      subject { described_class.new(n: 10) }
+      subject { described_class.new(10) }
 
       it 'returns 3628800' do
         subject.factorial
-        expect(subject).to eql(3628800)
+        expect(subject.factorial).to eql(3628800)
       end
     end
   end
